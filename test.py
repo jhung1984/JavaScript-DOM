2737e52dbcf91fafe9e97c112ab8c3ba5264b392
jhung1984 <jhung1984@gmail.com>
2019-04-28 10:31:00 +0800

    123

--- test.py
+++ test.py
@@ -79,3 +79,43 @@ while (count <= n):
     sum += count
     count += 1
 print("1到 %d 之和为:%d" % (n,sum))
+
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+class Employee:
+    empCount = 0
+
+    def __init__(self, name,salary):
+        self.name = name
+        self.salary =salary
+        Employee.empCount += 1
+    
+    def displayCount(self):
+        print("Total Employee %d" % Employee.empCount)
+    
+    def displayEmployee(self):
+        print("Name:",self.name,",Salary:",self.salary)
+
+emp1 = Employee("ZARA",2000)
+emp2 = Employee("Manni",5000)
+emp1.displayEmployee()
+emp2.displayEmployee()
+print("Total Employee %d" % Employee.empCount)
+
+class Point:
+    def __init__(self,x = 0,y = 0):
+        self.x = x
+        self.y = y
+    def __del__(self):
+        class_name = self.__class__.__name__
+        print(class_name,"销毁")
+
+pt1 = Point()
+pt2 = pt1
+pt3 = pt1
+print(id(pt1),id(pt2),id(pt3))
+del pt1
+del pt2
+del pt3
+
